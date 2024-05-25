a5c899ed8e5612587d757072f1a119332e15be15
Otavio Migliavacca Madalosso <otaviomadalosso@gmail.com>
2024-05-25 15:34:27 -0300

    3.7

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	gamelogic "github.com/Madalosso/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/Madalosso/learn-pub-sub-starter/internal/pubsub"
+	routing "github.com/Madalosso/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connection, err := amqp.Dial(routing.BrokerUrl)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer connection.Close()
+
+	fmt.Println("Connection established")
+
+	username, err := gamelogic.ClientWelcome()
+
+	_, _, err = pubsub.DeclareAndBind(connection, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, username), routing.PauseKey, 1)
+
+	signalChan := make(chan os.Signal, 1)
+	//subscribe?
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Printf("\nConnection shutdown\n")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	gamelogic "github.com/Madalosso/learn-pub-sub-starter/internal/gamelogic"
+	pubsub "github.com/Madalosso/learn-pub-sub-starter/internal/pubsub"
+	routing "github.com/Madalosso/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connection, err := amqp.Dial(routing.BrokerUrl)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer connection.Close()
+
+	fmt.Println("Connection established")
+
+	serverChan, err := connection.Channel()
+	if err != nil {
+		fmt.Println("Error while creating connection channel", err)
+	}
+	err = pubsub.PublishJSON(serverChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		fmt.Println("Error while publishing game state to pubsub", err)
+	}
+
+	gamelogic.PrintServerHelp()
+	for {
+		input := gamelogic.GetInput()
+		if len(input) != 0 {
+			command := input[0]
+			switch command {
+			case "pause":
+				fmt.Println("The game is being paused")
+				err = pubsub.PublishJSON(serverChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+				if err != nil {
+					fmt.Println("Error while publishing game state to pubsub", err)
+				}
+			case "resume":
+				fmt.Println("The game is being resumed")
+				err = pubsub.PublishJSON(serverChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+				if err != nil {
+					fmt.Println("Error while publishing game state to pubsub", err)
+				}
+			case "quit":
+				fmt.Println("Exiting the game")
+				break
+			default:
+				fmt.Printf("The command `%s` isn't valid\n", command)
+			}
+		}
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	//subscribe?
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Printf("\nConnection shutdown\n")
 }
